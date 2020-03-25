aee2e8138f64e5b6e76cc0035df858d477e50755
Tong Dang Tinh <tinhtongdang@gmail.com>
2020-03-25 16:28:28 +0700

    Add category, modify devise's view, modify header

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,6 @@
 class Book < ApplicationRecord
+  belongs_to :user
+  belongs_to :category
   validates :title, presence: true, length: { maximum: 255 }
   validates :author, presence: true, length: { maximum: 255 }
   validates :description, presence: true
